7be9fa9321b6a1aea453674da13ec6351807b3b0
Mahashribalakrishnan <230160.cb@rmkec.ac.in>
2026-02-15 15:24:52 +0530

    Initial commit

---
+++ Code_parser.py
@@ -0,0 +1,20 @@
+import ast
+code = """
+import math
+side = 10
+area = side*side
+"""
+
+tree = ast.parse(code)
+print(ast.dump(tree, indent=2))
+
+for node in ast.walk(tree):
+    if isinstance(node, ast.Import):
+        for alias in node.names:
+            print(f" Import Node: Library name '{alias.name}'")
+
+    if isinstance(node, ast.Name):
+        if isinstance(node.ctx, ast.Store):
+            print(f" Variable Node (Creation): Variable name '{node.id}'")
+        elif isinstance(node.ctx, ast.Load):
+            print(f" Variable Node (Usage): '{node.id}' is being used")
