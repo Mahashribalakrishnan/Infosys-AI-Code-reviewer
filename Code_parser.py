c7fc47b4289faedc4a805a13424623b43c604df4
Mahashribalakrishnan <230160.cb@rmkec.ac.in>
2026-02-16 20:19:39 +0530

    Added exception handling and user input

--- Code_parser.py
+++ Code_parser.py
@@ -1,21 +1,23 @@
 import ast
-code = """
-import math
-side = 10
-area = side*side
-"""
+lines = []
+while True:
+    line = input()
+    if line.strip() == "end":
+        break
+    lines.append(line)
 
-tree = ast.parse(code)
-print(ast.dump(tree, indent=2))
-print(ast.unparse(tree))
+code = "\n".join(lines)
 
-for node in ast.walk(tree):
-    if isinstance(node, ast.Import):
-        for alias in node.names:
-            print(f" Import Node: Library name '{alias.name}'")
-
-    if isinstance(node, ast.Name):
-        if isinstance(node.ctx, ast.Store):
-            print(f" Variable Node (Creation): Variable name '{node.id}'")
-        elif isinstance(node.ctx, ast.Load):
-            print(f" Variable Node (Usage): '{node.id}' is being used")
+try:
+    tree = ast.parse(code)
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for alias in node.names:
+                print(f" Import Node: Library name '{alias.name}'")
+        if isinstance(node, ast.Name):
+            if isinstance(node.ctx, ast.Store):
+                print(f" Variable Node (Creation): Variable name '{node.id}'")
+            elif isinstance(node.ctx, ast.Load):
+                print(f" Variable Node (Usage): '{node.id}' is being used")
+except SyntaxError as e:
+    print(f"Syntax Error: {str(e)}")
